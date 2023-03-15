a9ba919d146fa4d35402d06ef37df0936c9c7544
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-03-15 21:47:17 +0000

    Added Header/NavBar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from './components/Header'
+// import Service from './components/service'
+// import About from './components/About'
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* 
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} /> 
+        */}
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 
