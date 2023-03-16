077a8378549ed6c58f9476096641e0176812d42d
kelenyathi <kele.nyathi@outlook.com>
2023-03-16 20:37:55 +0000

    changes to about.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <div className="jumbotron bg-light">
+      <div className="wrapper__jumbotron d-flex">
+        <div className="jumbotron__content d-flex flex-column">
+          <h1>About Us</h1>
+          <p>We are meal master</p>
+          <div className="d-flex gap-2">
+            <p>
+            </p>
+            <p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+
+
+    
   )
 }
 
