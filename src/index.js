63f3014bda389b3044e6ce7610fd7cfbd9f80082
kelenyathi <kele.nyathi@outlook.com>
2023-03-17 19:52:09 +0000

    made changes to about page

--- src/index.js
+++ src/index.js
@@ -17,11 +17,14 @@ ReactDOM.render(
         <Route path="/" element={<App />} />
         {/* 
         <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
+       
          */}
         <Route path="/contact" element={<Contact />} /> 
-       
+        <Route path="/about" element={<About />} />
       </Routes>
+      <About>
+
+      </About>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
